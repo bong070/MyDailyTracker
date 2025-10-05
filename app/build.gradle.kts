521793923538d0849a7560ac1305111ea1b08d73
BK Software <bongkyus@hotmail.com>
2025-10-05 04:15:09 -0400

    Fixed issue with onboarding message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.bbks.mydailytracker"
         minSdk = 24
         targetSdk = 35
-        versionCode = 27
-        versionName = "1.1.0"
+        versionCode = 29
+        versionName = "1.1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
